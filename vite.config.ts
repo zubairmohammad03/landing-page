11fa1ae0a4b356f25209f752e42ef34f110e23d3
Mohd Zubair <zubairmohammad678@email.com>
2026-01-27 01:37:41 +0530

    Fix Vite base path for Cloud Run

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',   // ⭐ THIS IS THE IMPORTANT LINE
+
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
